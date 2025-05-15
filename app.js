96831fa453ef28e334305119dc202adc08387b6c
asaraya <asaraya153@hotmail.com>
2025-05-14 20:01:38 -0600

    Prueba auto-deploy: cambio de saludo

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! on Node.js Express v2')
+  res.send('Hello World! on Node.js Express v5')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-// prueba auto-deploy
